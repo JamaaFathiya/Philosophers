175f9c08ea518c432519d1bfdbd47c6e3201b56b
Fathiyat olatokunbo Jamia <fathjami@e2r9p14.1337.ma>
2022-03-04 11:49:13 +0100

    vri

--- philo.c
+++ philo.c
@@ -4,8 +4,8 @@ long long getmili(void)
 {
     struct timeval current_time;
     gettimeofday(&current_time, NULL);
-    // return (current_time.tv_sec * 1000 + current_time.tv_usec / 1000);
-    return (current_time.tv_sec * 100);
+    return (current_time.tv_sec * 1000 + current_time.tv_usec / 1000);
+    //return (current_time.tv_sec * 100);
 }
 
 void  take_a_fork(t_phil *philo, int id, char *hand)
@@ -24,8 +24,8 @@ void eat(t_phil *philo)
     take_a_fork(philo, philo->id, "right");
     take_a_fork(philo, (philo->id + 1) % philo->arg->nbr_of_philo, "left");
     printf("%lli Philospher %i is eating\n",getmili(), philo->id + 1);
-    // usleep(philo->arg->t_eat);
-    sleep(1);
+     usleep(philo->arg->t_eat * 1000);
+    //sleep(1);
     put_down(philo,  philo->id);
     put_down(philo, (philo->id + 1) % philo->arg->nbr_of_philo);
     philo->state = 1;
@@ -35,6 +35,6 @@ void sleip(t_phil *philo)
 {
    
     printf("%lli Philospher %i is sleeping\n",getmili(), philo->id + 1);
-    // usleep(philo->arg->t_sleep);
-    sleep(1);
+    usleep(philo->arg->t_sleep * 1000);
+    //sleep(1);
 }
